814b388ec8d847b9b32ecb48154ee974142e8ba0
Krystian Carter <krystian.carter18@gmail.com>
2025-05-13 18:47:52 -0400

    Cleaned File

--- src/main/java/com/mystichorizonsmc/playerexrebirth/network/packet/ClientPrestigeRequest.java
+++ src/main/java/com/mystichorizonsmc/playerexrebirth/network/packet/ClientPrestigeRequest.java
@@ -1,17 +1,11 @@
 package com.mystichorizonsmc.playerexrebirth.network.packet;
 
-import io.wispforest.owo.network.ClientAccess;
 import io.wispforest.owo.network.ServerAccess;
-import io.wispforest.owo.network.serialization.RecordSerializer;
-import net.minecraft.resources.ResourceLocation;
 import com.mystichorizonsmc.playerexrebirth.PlayerExRebirth;
 import com.mystichorizonsmc.playerexrebirth.prestige.PrestigeManager;
 import net.minecraft.server.level.ServerPlayer;
 
 public record ClientPrestigeRequest() {
-
-    public static final ResourceLocation ID = PlayerExRebirth.id("client_prestige_request");
-    public static final RecordSerializer<ClientPrestigeRequest> SERIALIZER = RecordSerializer.create(ClientPrestigeRequest.class);
 
     public static void handle(ClientPrestigeRequest packet, ServerAccess access) {
         ServerPlayer player = access.player();
