e295eb9f83544518ea8ce76a0969e45a483d9185
Krystian Carter <krystian.carter18@gmail.com>
2025-05-13 18:50:04 -0400

    Created a central system for handling the Components and OWO data.

--- src/main/java/com/mystichorizonsmc/playerexrebirth/component/ModComponents.java
+++ src/main/java/com/mystichorizonsmc/playerexrebirth/component/ModComponents.java
@@ -3,13 +3,28 @@ package com.mystichorizonsmc.playerexrebirth.component;
 import com.mystichorizonsmc.playerexrebirth.PlayerExRebirth;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.minecraft.resources.ResourceLocation;
 
-public class ModComponents {
+
+
+public class ModComponents implements EntityComponentInitializer {
+
     public static final ComponentKey<PrestigeComponent> PRESTIGE =
             ComponentRegistry.getOrCreate(
                     new ResourceLocation(PlayerExRebirth.MOD_ID, "prestige"),
                     PrestigeComponent.class
             );
+
+    @Override
+    public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
+        registry.registerForPlayers(
+                PRESTIGE,
+                player -> new DefaultPrestigeComponent(),
+                RespawnCopyStrategy.ALWAYS_COPY
+        );
+    }
 }
