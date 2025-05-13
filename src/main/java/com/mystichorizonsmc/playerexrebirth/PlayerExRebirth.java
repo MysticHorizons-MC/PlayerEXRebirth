8a2acea76e8da33ec71580cd1afebf8d0ef63897
Krystian Carter <krystian.carter18@gmail.com>
2025-05-13 18:50:52 -0400

    Switched owo network to `playerexrebirthnet` to prevent possible desyncs.

--- src/main/java/com/mystichorizonsmc/playerexrebirth/PlayerExRebirth.java
+++ src/main/java/com/mystichorizonsmc/playerexrebirth/PlayerExRebirth.java
@@ -2,7 +2,6 @@ package com.mystichorizonsmc.playerexrebirth;
 
 import com.mystichorizonsmc.playerexrebirth.config.PrestigeConfig;
 import com.mystichorizonsmc.playerexrebirth.network.PrestigeSyncS2CPacket;
-import com.mystichorizonsmc.playerexrebirth.network.packet.ClientPrestigeRequest;
 import io.wispforest.owo.network.OwoNetChannel;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
@@ -13,14 +12,17 @@ public class PlayerExRebirth implements ModInitializer {
 
     public static final String MOD_ID = "playerexrebirth";
     public static final Logger LOGGER = LoggerFactory.getLogger("PlayerEx Rebirth");
-    public static final OwoNetChannel NETWORK = OwoNetChannel.create(id("main"));
+    public static final OwoNetChannel NETWORK = OwoNetChannel.create(id("playerexrebirthnet"));
 
     @Override
     public void onInitialize() {
         LOGGER.info("[{}] Initializing...", MOD_ID);
 
-        NETWORK.registerServerbound(ClientPrestigeRequest.class, ClientPrestigeRequest::handle);
+        // Network packets
+        NetworkHandler.registerCommon(NETWORK);
         PrestigeSyncS2CPacket.registerJoinSync();
+
+        // Config
         PrestigeConfig.load();
     }
 
