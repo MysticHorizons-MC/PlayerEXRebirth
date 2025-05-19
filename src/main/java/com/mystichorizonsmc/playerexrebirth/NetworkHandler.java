746ae64ebd1272a94f339688c12e425e396e6bc7
Krystian Carter <krystian.carter18@gmail.com>
2025-05-18 22:32:13 -0400

    Removed the dummy packet handler

--- src/main/java/com/mystichorizonsmc/playerexrebirth/NetworkHandler.java
+++ src/main/java/com/mystichorizonsmc/playerexrebirth/NetworkHandler.java
@@ -2,7 +2,6 @@ package com.mystichorizonsmc.playerexrebirth;
 
 
 import com.mystichorizonsmc.playerexrebirth.network.packet.ClientPrestigeRequest;
-import com.mystichorizonsmc.playerexrebirth.network.packet.PrestigeToastS2CPacket;
 import io.wispforest.owo.network.OwoNetChannel;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
@@ -12,10 +11,5 @@ public class NetworkHandler {
     public static void registerCommon(OwoNetChannel channel) {
         // Serverbound packets must be registered on both sides
         channel.registerServerbound(ClientPrestigeRequest.class, ClientPrestigeRequest::handle);
-
-        // Only register the dummy clientbound handler if on dedicated server
-        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
-            channel.registerClientbound(PrestigeToastS2CPacket.class, (packet, access) -> {});
-        }
     }
 }
