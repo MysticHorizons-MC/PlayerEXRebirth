abc4fa22e279e5a3c66a591fc4b1f5c488f68b02
Krystian Carter <krystian.carter18@gmail.com>
2025-05-18 22:31:51 -0400

    Added effective max level getters and removed unneeded getters

--- src/client/java/com/mystichorizonsmc/playerexrebirth/client/data/ClientPrestigeData.java
+++ src/client/java/com/mystichorizonsmc/playerexrebirth/client/data/ClientPrestigeData.java
@@ -2,24 +2,16 @@ package com.mystichorizonsmc.playerexrebirth.client.data;
 
 public class ClientPrestigeData {
 
-    private static int maxPrestigeLevel = 100;
     private static int currentPrestigeLevel = 0;
+    private static int effectiveMaxLevel = 100;
     private static boolean toastShown = false;
 
-    public static void setMaxPrestigeLevel(int level) {
-        maxPrestigeLevel = level;
-    }
-
-    public static int getMaxPrestigeLevel() {
-        return maxPrestigeLevel;
+    public static int getCurrentPrestigeLevel() {
+        return currentPrestigeLevel;
     }
 
     public static void setCurrentPrestigeLevel(int level) {
         currentPrestigeLevel = level;
-    }
-
-    public static int getCurrentPrestigeLevel() {
-        return currentPrestigeLevel;
     }
 
     public static boolean shouldShowToast() {
@@ -28,5 +20,13 @@ public class ClientPrestigeData {
 
     public static void setToastShown() {
         toastShown = true;
+    }
+
+    public static int getEffectiveMaxLevel() {
+        return effectiveMaxLevel;
+    }
+
+    public static void setEffectiveMaxLevel(int level) {
+        effectiveMaxLevel = level;
     }
 }
