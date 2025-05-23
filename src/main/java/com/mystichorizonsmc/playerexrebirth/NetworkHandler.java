f898cb2eae8a0a55117be550378a9f3dfd80766c
Krystian Carter <krystian.carter18@gmail.com>
2025-05-22 23:44:01 -0400

    Added Client sync because I forgot...

--- src/main/java/com/mystichorizonsmc/playerexrebirth/NetworkHandler.java
+++ src/main/java/com/mystichorizonsmc/playerexrebirth/NetworkHandler.java
@@ -1,6 +1,7 @@
 package com.mystichorizonsmc.playerexrebirth;
 
 
+import com.mystichorizonsmc.playerexrebirth.network.PrestigeSyncS2CPacket;
 import com.mystichorizonsmc.playerexrebirth.network.packet.ClientPrestigeRequest;
 import io.wispforest.owo.network.OwoNetChannel;
 import net.fabricmc.api.EnvType;
@@ -11,5 +12,6 @@ public class NetworkHandler {
     public static void registerCommon(OwoNetChannel channel) {
         // Serverbound packets must be registered on both sides
         channel.registerServerbound(ClientPrestigeRequest.class, ClientPrestigeRequest::handle);
+        channel.registerClientbound(PrestigeSyncS2CPacket.class, PrestigeSyncS2CPacket::handle);
     }
 }
